8dd0bde3fd9f6e38ec2d713cbdfdd8f861d71834
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-12-28 23:58:45 +0300

    Separates Poster component from Card component

--- src/js/components/card/card.tsx
+++ src/js/components/card/card.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import {Item} from "../../types";
 
 import CardStatus from "../card-status/card-status";
+import Poster from "../poster/poster";
 
 interface Props {
   item: Item;
@@ -12,66 +13,26 @@ interface Props {
 
 const Card = (props: Props):React.ReactElement => {
   const {item, isActive, onClick} = props;
-  const {title, weight, description, amount, consist} = item;
-  const [isFirstSelect, setIsFirstSelect] = React.useState(false);
-  const posterRef = React.useRef(null);
+  const {title, description, amount} = item;
 
   let cardStateClass = ``;
-  let posterStateClass = ``;
 
   if (amount) {
     if (isActive) {
-      posterStateClass = isFirstSelect ? ` poster--selected-first` : ` poster--selected`;
       cardStateClass = ` card--selected`;
-    } else {
-      posterStateClass = ` poster--unselected`;
     }
   } else {
-    posterStateClass = ` poster--disabled`;
     cardStateClass = ` card--disabled`;
   }
 
-  const handlePointerLeave = ():void => {
-    setIsFirstSelect(false);
-    posterRef.current.blur();
-  };
-
-  const handlePosterClick = (evt):void => {
-    if (!isActive) {
-      setIsFirstSelect(true);
-    }
-    onClick(evt, item);
-  };
 
   return (
     <article className={`cards_card card${cardStateClass}`}>
-      <div ref={posterRef} onPointerLeave={isFirstSelect ? handlePointerLeave : null} onClick={amount ? handlePosterClick : null} className={`card__poster poster${posterStateClass}`} tabIndex={amount ? 1 : -1}>
-        <div className="poster__content">
-          <p className="poster__text">
-            <span className="poster__phrase1">Сказочное заморское яство</span>
-            <span className="poster__phrase2">Котэ не одобряет?</span>
-          </p>
-          <h2 className="poster__title">
-            Нямушка
-          </h2>
-          <p className="poster__taste">{title}</p>
-          <ul className="poster__consist consist">
-            {
-              consist.map((it) => {
-                return (
-                  <li key={it.name} className="consist__item">
-                    {it.value ? <><b>{it.value}</b>{` ${it.name}`}</> : it.name}
-                  </li>
-                );
-              })
-            }
-          </ul>
-        </div>
-        <div className="poster__weight weight">
-          <span className="weight__value">{weight}</span>
-          <span className="weight__measure">кг</span>
-        </div>
-      </div>
+      <Poster
+        isActive={isActive}
+        item={item}
+        onClick={onClick}
+      />
       <CardStatus
         isAvailable={!!amount}
         title={title}
---
+++ src/js/components/poster/poster.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+
+import {Item} from "../../types";
+
+interface Props {
+  item: Item;
+  isActive: boolean;
+  onClick: (evt: React.MouseEvent, item: Item) => void;
+}
+
+const Poster = (props: Props):React.ReactElement => {
+  const {item, isActive, onClick} = props;
+  const {title, weight, amount, consist} = item;
+  const [isFirstSelect, setIsFirstSelect] = React.useState(false);
+  const posterRef = React.useRef(null);
+
+  let posterStateClass = ``;
+
+  if (amount) {
+    if (isActive) {
+      posterStateClass = isFirstSelect ? ` poster--selected-first` : ` poster--selected`;
+    } else {
+      posterStateClass = ` poster--unselected`;
+    }
+  } else {
+    posterStateClass = ` poster--disabled`;
+  }
+
+  const handlePointerLeave = ():void => {
+    setIsFirstSelect(false);
+    posterRef.current.blur();
+  };
+
+  const handlePosterClick = (evt):void => {
+    if (!isActive) {
+      setIsFirstSelect(true);
+    }
+    onClick(evt, item);
+  };
+
+  return (
+    <div ref={posterRef} onPointerLeave={isFirstSelect ? handlePointerLeave : null} onClick={amount ? handlePosterClick : null} className={`card__poster poster${posterStateClass}`} tabIndex={amount ? 1 : -1}>
+      <div className="poster__content">
+        <p className="poster__text">
+          <span className="poster__phrase1">Сказочное заморское яство</span>
+          <span className="poster__phrase2">Котэ не одобряет?</span>
+        </p>
+        <h2 className="poster__title">
+          Нямушка
+        </h2>
+        <p className="poster__taste">{title}</p>
+        <ul className="poster__consist consist">
+          {
+            consist.map((it) => {
+              return (
+                <li key={it.name} className="consist__item">
+                  {it.value ? <><b>{it.value}</b>{` ${it.name}`}</> : it.name}
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+      <div className="poster__weight weight">
+        <span className="weight__value">{weight}</span>
+        <span className="weight__measure">кг</span>
+      </div>
+    </div>
+  );
+};
+
+export default Poster;
